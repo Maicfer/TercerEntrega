283e41aec22516e9419c94f20aacf6e39758fd35
Maicfer <mc.maicfer@gmail.com>
2025-04-20 23:54:13 -0500

    Corregir Travel_the_World/Travel_the_World/settings.py.yaml

--- Travel_the_World/Travel_the_World/settings.py
+++ Travel_the_World/Travel_the_World/settings.py
@@ -119,6 +119,8 @@ AUTH_PASSWORD_VALIDATORS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
